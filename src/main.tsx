3d3292286bc17455edab20f6e79e3385cf6af495
Elvaceishim <theelvace@gmail.com>
2025-06-09 07:59:48 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { initializeStorage } from './lib/supabase';
-
-// Initialize Supabase storage on app start
-initializeStorage();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
