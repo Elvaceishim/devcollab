3beabbf4e1b6f709cf394e23e0d11ed6791d8442
Elvaceishim <anselmelvis62@gmail.com>
2025-06-09 21:45:39 +0100

    fix: update routing configuration for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,13 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+  publicDir: 'public',
 });
