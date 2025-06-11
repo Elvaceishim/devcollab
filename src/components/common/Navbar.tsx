62c369ed2901c4c3941253e73d41d4a3c15587ac
Elvaceishim <theelvace@gmail.com>
2025-06-11 16:05:56 +0100

    Fix missing useNavigate import in Navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Menu, X, Moon, Sun, Code2 } from 'lucide-react';
 import { useAuth } from '../../contexts/AuthContext';
 
